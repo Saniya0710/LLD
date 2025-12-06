83969e48044dc867bd76ec0cc191d26d0b79d97c
Saniya0710 <saniyatabassum6303@gmail.com>
2025-12-06 23:48:38 +0530

    Initial commit: Java Maven project with HelloWorld sample

---
+++ lld/src/main/java/com/example/basicsv1/singleresponsibility/solution/SolutionDemo.java
@@ -0,0 +1,11 @@
+package com.example.basicsv1.singleresponsibility.solution;
+
+public class SolutionDemo {
+    
+    public static void main(String[] args) {
+        System.out.println("Demonstrating Single Responsibility Principle Solution");
+    }
+}
+
+
+
